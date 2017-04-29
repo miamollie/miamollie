eae04fa3720ea4cb24bbd645d6056598bf693597
Mia <miamollie@gmail.com>
2017-04-29 18:48:11 +1000

    add new images and update gallery and contact

--- includes/gallery.php
+++ includes/gallery.php
@@ -6,97 +6,52 @@
 		// convert the string to a json object
 		$jfo = json_decode($json_file);
 
+		function renderImages($images)
+			{
+				foreach ($images as $image) {
+					$img = $image->img;
+					$title = $image->title;
+					echo "<li><a title=" . $title . " href=" .  $img  ."></a></li>";
+				}
+		}
 	?>
-
-
-	<div id="gallery" class="container-fullscreen">
-		<h2>Gallery</h2>
-
-		<div class="container">
-			<div class="col-sm-6">
+	<div id="gallery" class="container-fullscreen flex fRow-lg fColumn-sm">
+		<div class="full-lg full-sm flex fRow-lg fColumn-sm">
+			<div class="half-lg full-sm flex fColumn-lg fColumn-sm ">
 				<div class="gallery-item" id="solo">
 					<h3>Solo Work</h3>
 					<ul>
 						<?php
-							$solos = $jfo->solo;
-							foreach ($solos as $solo) {
-								$img = $solo->img;
-								$title = $solo->title;
-								echo "<li><a title=" . $title . " href=" .  $img  ."></a></li>";
-							}
+							$images = $jfo->solo;
+							renderImages($images)
 						?>
-
 					</ul>
 				</div>
 				<div class="gallery-item" id="shoot">
 					<h3>Photoshoots</h3>
 					<ul>
 						<?php
-							$shoots = $jfo->shoot;
-
-							foreach ($shoots as $shoot) {
-								$img = $shoot->img;
-								$title = $shoot->title;
-								echo "<li><a title=" . $title . " href=" .  $img  ."></a></li>";
-							}
-
+							$images = $jfo->shoot;
+							renderImages($images)
 						?>
-
-					</ul>
 				</div>
-
 			</div>
-			<div class="col-sm-6">
+			<div class="half-lg full-sm flex fColumn-lg fColumn-sm ">
 				<div class="gallery-item" id="reel">
 					<h3>Show Reel</h3>
 					<ul>
 						<li><a class="video" title="Showreel" href="https://vimeo.com/215285721"><i class="fa fa-play-circle-o"></i></a></li>
 					</ul>
-
-
 				</div>
-
-				<div class="col-sm-6">
-					<div class="gallery-item" id="outdoor">
-						<h3>Outdoor</h3>
-						<ul>
-
-							<?php
-								$outdoors = $jfo->out;
-
-								foreach ($outdoors as $out) {
-									$img = $out->img;
-									$title = $out->title;
-									echo "<li><a title=" . $title . " href=" .  $img  ."></a></li>";
-								}
-
-							?>
-
-						</ul>
-
-					</div>
-				</div>
-				<div class="col-sm-6">
-					<div class="gallery-item" id="double">
-						<h3>Partner Work</h3>
-						<ul>
-
-							<?php
-								$doubles = $jfo->double;
-
-								foreach ($doubles as $double) {
-									$img = $double->img;
-									$title = $double->title;
-									echo "<li><a title=" . $title . " href=" .  $img  ."></a></li>";
-								}
-
-							?>
-						</ul>
-
-					</div>
-				</div>
+				<div class="gallery-item" id="double">
+				<h3>Duo</h3>
+				<ul>
+					<?php
+						$images = $jfo->double;
+						renderImages($images)
+					?>
+				</ul>
 			</div>
-
+			</div>
 		</div>
-
 	</div>
